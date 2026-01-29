822c2aa15bb63719479a04110a651af494fbb4d3
DominicDev <s0l0estudi4r@gmail.com>
2026-01-29 15:44:38 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'NEON TREES';
+export const nameSongs = [
+	"ANIMAL", /*1*/
+	"EVERYBODY TALKS", /*2*/
+	"1983", /*3*/
+	"SLEEPING WITH A FRIEND", /*4*/
+	"LESSON IN LOVE ", /*5*/
+	"LOVE AND AFFECTION ", /*6*/
+	"YOUR SURRENDER", /*7*/
+	"MOVING IN THE DARK", /*8*/
+	"IN THE NEXT ROOM", /*9*/
+	"SINS OF MY MOUTH", /*10*/
+	"FIRST THINGS FIRST", /*11*/
+	"VOICES IN THE HALLS", /*12*/
+	"MAD LOVE ", /*13*/
+	"TEXT ME IN THE MORNING", /*14*/
+	"WEEKEND", /*15*/
+	"TRUST  ", /*16*/
+	"LIVING IN ANOTHER WORLD", /*17*/
+	"SONGS I CAN’T LISTEN TO", /*18*/
+	"CLOSE TO YOU", /*19*/
+	"CALLING MY NAME", /*20*/
+]
